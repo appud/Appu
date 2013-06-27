8dbe0aac4a8b2697170b511629f739df6240e33c
sathyamgit <sathyamgit@gmail.com>
2013-06-27 22:36:33 +0530

    1st commit

---
+++ Appu/src/com/olabsys/appu/First.java
@@ -0,0 +1,14 @@
+package com.olabsys.appu;
+
+public class First {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("File created...");
+
+	}
+
+}
